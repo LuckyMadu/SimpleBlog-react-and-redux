56cb2dc33e4f6b8e4d9828eaad20a98f6b648477
unknown <you@example.com>
2018-11-16 19:26:21 +0530

    basic blog

---
+++ bloggy/src/components/Contact.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Contact = props => {
+  //console.log(props);
+  /* setTimeout(() => {
+    props.history.push("/about");
+  }, 2000); */
+  
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+        mollitia est? Qui quisquam, aspernatur soluta impedit blanditiis
+        ratione, dignissimos vero dicta debitis suscipit recusandae esse quod
+        est velit deserunt inventore.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
